9011f8f213a1d3c6251bd5020a5763b008db904c
Lefteris-B <62570233+Lefteris-B@users.noreply.github.com>
2020-06-03 19:18:22 +0300

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,20 +1,22 @@
-int ENA = 3; //Δεξιοί κινητήρες
-int IN1 = 1; 
-int IN2 = 2;
+int ENA = 11; //Δεξιοί κινητήρες
+int IN1 = 10; 
+int IN2 = 9;
 
 int ENB = 6; //Αριστεροί κινητήρες
-int IN3 = 4;
-int IN4 = 5;
+int IN3 = 5;
+int IN4 = 3;
+
+int RSensor = A4;  //mple    IR Sensors
+int LSensor = A5;  //prasino
 
 //Ταχύτητα Κινητήρων
-#define ENASpeed 100 
-#define ENBSpeed 100
+#define ENASpeed 150   
+#define ENBSpeed 180
 
 //Αρχικοποίηση μεταβλητών
-int Sensor1 = 0;
-int Sensor2 = 0;
-int Sensor3 = 0;
-int Sensor4 = 0;
+int l_sens = 0;
+int r_sens = 0;
+
 
 void setup() {
 //Αρχικοποίηση εισόδων και εξόδων
@@ -27,10 +29,9 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
-  pinMode(Sensor1, INPUT);
-  pinMode(Sensor2, INPUT);
-  pinMode(Sensor3, INPUT);
-  pinMode(Sensor4, INPUT);
+  pinMode(RSensor, INPUT);
+  pinMode(LSensor, INPUT);
+  
   
 }
 
@@ -41,26 +42,25 @@ void loop(){
   analogWrite(ENB, ENBSpeed);
 
   //Ανάνωση αισθητήρων
-  Sensor1 = digitalRead(8);
-  Sensor2 = digitalRead(9);
-  Sensor3 = digitalRead(10);
-  Sensor4 = digitalRead(11);
+  r_sens = digitalRead(RSensor);
+  l_sens = digitalRead(LSensor);
+  
 
   //Θέτουμε προυποθέσεις για κίνηση ΜΠΡΟΣΤΑ,ΔΕΞΙΑ,ΑΡΙΣΤΕΡΑ,ΣΤΑΜΑΤΑ
 
-  if(Sensor4 == HIGH && Sensor3 == HIGH && Sensor2 == LOW && Sensor1 == LOW){
+  if(r_sens == HIGH && l_sens == LOW){
     turn_left();
   }
   
-  else if (Sensor4 == LOW && Sensor3 == LOW && Sensor2 == HIGH && Sensor1 == HIGH){
+  else if (r_sens == LOW && l_sens == HIGH){
     turn_right();
   }
   
-  else if(Sensor4 == LOW && Sensor3 == HIGH && Sensor2 == HIGH && Sensor1 == LOW){
+  else if(r_sens == LOW && l_sens == LOW){
     move_f();
   }
   
-  else if (Sensor4 == HIGH && Sensor3 == HIGH && Sensor2 == HIGH && Sensor1 == HIGH){
+  else if (r_sens == HIGH && l_sens == HIGH){
      stop_m();
   }
 }
@@ -77,23 +77,22 @@ void move_f(){    //ΜΠΡΟΣΤΑ
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
     
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
 }
 
 void turn_right() {    //Turn ΔΕΞΙΑ
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
 
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, HIGH);
 }
 
 void turn_left(){    //ΑΡΙΣΤΕΡΑ
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
 
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
 }
-
